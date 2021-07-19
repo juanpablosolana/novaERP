254a1459cd4d829a2262dd5987d33bd37538dc40
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-07-19 17:47:38 -0500

    modal ready to deploy

--- components/CfdiToJson/index.js
+++ components/CfdiToJson/index.js
@@ -7,17 +7,14 @@ export default function Cfdi({ cfdiUrl }) {
   const fetcher = (url) =>
     fetch(`https://cfditojson.herokuapp.com/?id=${cfdiUrl}`).then((r) => r.json());
   const { data } = useSWR('/api/data', fetcher)
- console.log(data);
+
 
   if (!data) return <div className={styles.spinner}></div>;
 
 
   return (
     <div>
-      <h2>Información de : {data.timbreFiscal.uuid.toUpperCase()}</h2>
-      <ul>
-
-      </ul>
+      <h2>{data}</h2>
     </div>
   );
 }
