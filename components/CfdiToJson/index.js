0e7ac408d1c6c360abe0beac51b3f228bf60dc6c
Juan Pablo Solana <juanpablosolana@gmail.com>
2021-07-30 11:28:02 -0500

    deploy API

--- components/CfdiToJson/index.js
+++ components/CfdiToJson/index.js
@@ -12,7 +12,7 @@ export default function Cfdi({ cfdiUrl }) {
   }, [])
 
   const fetcher = _ =>
-    fetch(`/api/xmlToJson/?id=${cfdiUrl}`).then((r) => r.json());
+    fetch(`https://nova-erp.vercel.app/api/xmlToJson/?id=${cfdiUrl}`).then((r) => r.json());
   let { data } = useSWR(mounted ? '/api/data' : null, fetcher)
 
 
